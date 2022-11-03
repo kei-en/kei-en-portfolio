a76dc13975e8e2a3d3216f5a7f591486a2b64e7c
Kei En <njuguna0karanja@gmail.com>
2022-11-04 01:52:11 +0300

    intro section

--- components/intro.js
+++ components/intro.js
@@ -1,14 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
+import { TiArrowDownThick } from 'react-icons/ti';
 
 export default function Intro() {
   return (
-    <div>
-        <h1 className={'font-monoton'}>Njuguna Karanja</h1>
-        <div>
-          <Image src='/joe1.jpg' alt='Image: Kei En' width={500} height={500} />
+    <div className={"h-screen bg-black text-white flex flex-col justify-around align-middle"}>
+        <h1 className={'font-monoton text-5xl text-center pt-16'}>Njuguna Karanja</h1>
+        <div className={"text-center"}>
+          <Image className={"rounded-full"} src='/joe1.jpg' alt='Image: Kei En' width={250} height={250} />
         </div>
-        <h3 className={"font-audiowide"}>web developer & designer</h3>
+        <h3 className={"font-audiowide text-center text-2xl"}>web developer & designer</h3>
+        <div className={"flex justify-center"}>
+          <TiArrowDownThick size={42}/> 
+          <h2 className={"text-gray-400 text-sm"}>more about me</h2>
+        </div>
     </div>
   )
 }
