319bd0b33216c95cd69fb0bcd250a59c494bffe8
kei-en <njuguna0karanja@gmail.com>
2024-05-02 23:20:00 +0300

    Add Footer component with animated links

---
+++ components/sections/Footer.js
@@ -0,0 +1,47 @@
+import { motion } from 'framer-motion';
+
+import useAnimateIn from '../../hooks/useAnimateIn';
+
+export default function Footer() {
+  const {
+    ref: linkRef,
+    ctrls: linkCtrls,
+    vars: linkVars,
+  } = useAnimateIn({
+    delay: 2,
+  });
+
+  return (
+    <div className="p-2 lg:w-3/4 lg:ml-auto lg:mr-auto">
+      <ul className="w-full inline-flex justify-around">
+        <motion.li
+          ref={linkRef}
+          initial="hidden"
+          animate={linkCtrls}
+          variants={linkVars}
+          className="uppercase underline underline-offset-2 hover:overline"
+        >
+          <a href="#">Resume</a>
+        </motion.li>
+        <motion.li
+          ref={linkRef}
+          initial="hidden"
+          animate={linkCtrls}
+          variants={linkVars}
+          className="uppercase underline underline-offset-2 hover:overline"
+        >
+          <a href="https://github.com/kei-en">Github</a>
+        </motion.li>
+        <motion.li
+          ref={linkRef}
+          initial="hidden"
+          animate={linkCtrls}
+          variants={linkVars}
+          className="uppercase underline underline-offset-2 hover:overline"
+        >
+          <a href="https://linkedin.com/in/kei-en">Linkedin</a>
+        </motion.li>
+      </ul>
+    </div>
+  );
+}
