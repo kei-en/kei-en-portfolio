8d404aa53122a0dd363e5216d6b6a894fdd57048
kei-en <njuguna0karanja@gmail.com>
2024-05-12 21:27:00 +0300

    Add ContentMapper component for mapping and rendering project sections

---
+++ components/ContentMapper.js
@@ -0,0 +1,29 @@
+import ProjectImage from './sections/ProjectImage';
+import ProjectImageGrid from './sections/ProjectImageGrid';
+import ProjectImageText from './sections/ProjectImageText';
+import ProjectProcessWork from './sections/ProjectProcessWork';
+import ProjectText from './sections/ProjectText';
+import ProjectVideo from './sections/ProjectVideo';
+
+export default function ContentMapper({ sections }) {
+  const ContentMap =
+    sections !== null &&
+    sections.map((section, index) => {
+      switch (section._type) {
+        case 'project_image':
+          return <ProjectImage data={section} key={index} />;
+        case 'project_image_grid':
+          return <ProjectImageGrid data={section} key={index} />;
+        case 'project_image_text':
+          return <ProjectImageText data={section} key={index} />;
+        case 'project_process_work':
+          return <ProjectProcessWork data={section} key={index} />;
+        case 'project_text':
+          return <ProjectText data={section} key={index} />;
+        case 'project_video':
+          return <ProjectVideo data={section} key={index} />;
+      }
+    });
+
+  return <>{ContentMap}</>;
+}
