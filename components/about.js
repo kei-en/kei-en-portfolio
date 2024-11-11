23d4bba6b8e69ff14b1ce86902c4bfc0769a5532
kei-en <njuguna0karanja@gmail.com>
2024-11-11 15:15:21 +0300

    fix: Update About component header and improve layout; add social links with typewriter effect

--- components/about.js
+++ components/about.js
@@ -6,7 +6,7 @@ import useAnimateIn from '../hooks/useAnimateIn';
 import Typewriter from 'typewriter-effect';
 
 export default function About({ inView }) {
-  const textHdr = 'Who?';
+  const textHdr = 'About';
 
   // Copy animation
   const {
@@ -22,15 +22,32 @@ export default function About({ inView }) {
     <div
       layout="true"
       style={
-        inView ? { opacity: 1 } : { position: 'sticky', top: '6%', zIndex: 2 }
+        inView ? { opacity: 1 } : { position: 'sticky', top: '10%', zIndex: 3 }
       }
       className={
-        ' h-[calc(100vh-4rem)] bg-grey w-[94%] m-auto mb-20 p-2 rounded-lg flex flex-col text-center overflow-hidden'
+        'h-[calc(100vh-7rem)] bg-grey w-[90%] m-auto mb-20 p-2 rounded-lg flex flex-col text-center overflow-hidden'
       }
     >
       <Title title={textHdr} />
       <div className="h-full lg:pt-5 pb-10">
-        <span className="text-xl">Hey, I'm</span>
+        <motion.p
+          className="lg:ml-auto lg:mr-auto lg:w-1/2 text-center md:text-lg text-sm leading-6"
+          ref={textRef}
+          initial="hidden"
+          animate={textCtrls}
+          variants={textVars}
+        >
+          I'm available for web projects and based in Nairobi, Kenya.
+          <br />I am skilled in developing user interfaces for web apps that are
+          easy to use and visually appealing while being optimised for both
+          desktop and mobile devices. I appreciate working with cross-functional
+          teams to produce high-quality products that go above and beyond
+          customer expectations. I am always looking to take on new challenges
+          and continue learning in order to stay up-to-date on the latest trends
+          and technologies in the field. Feel free to get in touch below if
+          you're interested in working with me.
+        </motion.p>
+        <span className="text-xl">Socials:</span>
         <Typewriter
           className="my-4"
           options={{
@@ -42,24 +59,6 @@ export default function About({ inView }) {
             loop: true,
           }}
         />
-        <motion.p
-          className="lg:ml-auto lg:mr-auto lg:w-1/2 text-center md:text-lg text-sm leading-6"
-          ref={textRef}
-          initial="hidden"
-          animate={textCtrls}
-          variants={textVars}
-        >
-          A web developer and designer - here's a bit more about my background.
-          I'm available for web projects and based in Nairobi, Kenya.
-          <br />I am skilled in developing user interfaces for web apps that are
-          easy to use and visually appealing while being optimised for both
-          desktop and mobile devices. I appreciate working with cross-functional
-          teams to produce high-quality products that go above and beyond
-          customer expectations. I am always looking to take on new challenges
-          and continue learning in order to stay up-to-date on the latest trends
-          and technologies in the field. Feel free to get in touch at the bottom
-          of this page if you're interested in working with me.
-        </motion.p>
       </div>
     </div>
   );
